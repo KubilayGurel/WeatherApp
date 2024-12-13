17e3bfc7ad4784439577f59fee5fc9ec271ac566
Kubilay Gürel <kubilay.kubi99@gmail.com>
2024-12-14 02:19:25 +0300

    syntax fixed.

--- app/src/main/java/com/kubilaygurel/weatherapp/data/remote/WeatherAPI.kt
+++ app/src/main/java/com/kubilaygurel/weatherapp/data/remote/WeatherAPI.kt
@@ -7,7 +7,7 @@ interface WeatherAPI {
 
     @GET("v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,relative_humidity_2m,weather_code,pressure_msl,wind_speed_10m")
     suspend fun getWeatherData (
-        @Query("latidude") lat: Double,
+        @Query("latitude") lat: Double,
         @Query("longitude") long: Double,
     ): WeatherDTO
 }
